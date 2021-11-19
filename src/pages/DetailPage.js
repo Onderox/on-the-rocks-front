b6ee71d6fe97fb433fd62157c2852b8e7995b800
degurii <sansam709@gmail.com>
2021-11-19 14:27:38 +0900

    feature: [#1] pages 디렉토리, 페이지별 파일 추가
    - 파일은 우선 div 텍스트만 렌더링하도록 함

---
+++ src/pages/DetailPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const DetailPage = props => {
+  return <div>Detail Page</div>;
+};
+
+DetailPage.propTypes = {};
+
+export default DetailPage;
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ErrorPage = props => {
+  return <div>Error Page</div>;
+};
+
+ErrorPage.propTypes = {};
+
+export default ErrorPage;
